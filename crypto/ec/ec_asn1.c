42f0d8dcdfa5e9fd5ec8540b6bb569b870bdda56
Matt Caswell <matt@openssl.org>
2015-03-19 10:16:32 +0000

    backport a4517be9e348634ac64f9cf093131e13e8c03e38
    Fix a failure to NULL a pointer freed on error.
    
    Reported by the LibreSSL project as a follow on to CVE-2015-0209
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/ec/ec_asn1.c
+++ crypto/ec/ec_asn1.c
@@ -1377,8 +1377,6 @@ EC_KEY *d2i_ECParameters(EC_KEY **a, const unsigned char **in, long len)
 			ECerr(EC_F_D2I_ECPARAMETERS, ERR_R_MALLOC_FAILURE);
 			return NULL;
 			}
-		if (a)
-			*a = ret;
 		}
 	else
 		ret = *a;
@@ -1386,8 +1384,13 @@ EC_KEY *d2i_ECParameters(EC_KEY **a, const unsigned char **in, long len)
 	if (!d2i_ECPKParameters(&ret->group, in, len))
 		{
 		ECerr(EC_F_D2I_ECPARAMETERS, ERR_R_EC_LIB);
+                if (a == NULL || *a != ret)
+                     EC_KEY_free(ret);
 		return NULL;
 		}
+
+        if (a)
+            *a = ret;
 
 	return ret;
 	}
